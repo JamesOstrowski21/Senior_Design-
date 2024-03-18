c53dbbf74c2369ab311476f29da893262d2b8441
James Ostrowski <jamesostrowski21@gmail.com>
2024-03-18 13:09:46 -0500

    import button functionality

--- PythonQt/userInterface.py
+++ PythonQt/userInterface.py
@@ -1,4 +1,5 @@
 from PySide6 import QtCore, QtWidgets
+from PySide6.QtWidgets import QFileDialog
 from PySide6.QtUiTools import QUiLoader
 
 loader = QUiLoader()
@@ -7,7 +8,18 @@ class UserInterface(QtCore.QObject):
     def __init__(self):
         super().__init__()
         self.ui = loader.load("uiFile/mainwindow.ui", None)
-        self.ui.setWindowTitle("My App")
+        self.ui.setWindowTitle("intelliTrack")
+ 
+        self.ui.file_import_button.clicked.connect(self.open_wav_file)
+        self.ui.decode_button.setEnabled(False)
+
+    @QtCore.Slot()
+    def open_wav_file(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self.ui, "Open Wav File", "", "Wav Files (*.wav)", options=options)
+        if file_name:
+            self.ui.file_type_label.setText(file_name)
+            self.ui.decode_button.setEnabled(True)
 
     def show(self):
         self.ui.show()
